a7ed099e2ad3f35a72f4197b764f94a154ea8fcf
enorvang <espen.norvang@gmail.com>
2020-02-19 09:51:03 +0100

    added premade startcode and tests

---
+++ dat110-project2-startcode/src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -0,0 +1,23 @@
+package no.hvl.dat110.iotsystem;
+
+import no.hvl.dat110.client.Client;
+import no.hvl.dat110.common.TODO;
+
+public class TemperatureDevice {
+	
+	private static final int COUNT = 10;
+	
+	public static void main(String[] args) {
+		
+		TemperatureSensor sn = new TemperatureSensor();
+		
+		// TODO - start
+		
+		// TODO - end
+		
+		System.out.println("Temperature device stopping ... ");
+		
+		throw new UnsupportedOperationException(TODO.method());
+		
+	}
+}
