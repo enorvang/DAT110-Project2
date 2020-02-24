d49ed035153338717fba708c18791eb660fc3af4
enorvang <espen.norvang@gmail.com>
2020-02-24 12:52:35 +0100

    finished implementation of broker.Dispatcher

--- dat110-project2-startcode/src/no/hvl/dat110/messages/PublishMsg.java
+++ dat110-project2-startcode/src/no/hvl/dat110/messages/PublishMsg.java
@@ -1,9 +1,6 @@
 package no.hvl.dat110.messages;
 
 public class PublishMsg extends Message {
-	
-	// TODO: 
-	// Implement objectvariables, constructor, get/set-methods, and toString method
 
     private String topic;
     private String message;
